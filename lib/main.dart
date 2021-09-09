0f1a4365ce65b5ec921e90f50232dc4aca7904b5
AneezaZafar19 <85999990+AneezaZafar19@users.noreply.github.com>
2021-09-09 18:56:22 +0500

    to print list through map instead of listview or listview.builder

--- lib/main.dart
+++ lib/main.dart
@@ -6,71 +6,111 @@ import 'package:flutter_practice/practice/identity_card.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: dataList(),
+      home: mapList(),
     ),
   );
 }
 
-//to call and perform actions on custom class
-class dataList extends StatefulWidget {
+//#############################################################################################################
+//to print list through map
+class mapList extends StatefulWidget {
   @override
-  _dataListState createState() => _dataListState();
+  _mapListState createState() => _mapListState();
 }
 
-class _dataListState extends State<dataList> {
+class _mapListState extends State<mapList> {
   List<S_data> data = [
     S_data(name: 'Aneeza Zafar', rollnum: '0261-BSCS-18'),
     S_data(name: 'Khadija Khalid', rollnum: '0265-BSCS-18'),
     S_data(name: 'Asma Fiaz', rollnum: '0273-BSCS-18'),
     S_data(name: 'Mahnoor Tariq', rollnum: '0227-BSCS-18'),
   ];
-  // @override
-  // void initState() {
-  //   // TODO: implement initState
-  //   S_data d = data[0];
-  //   print(d.name);
-  //   super.initState();
-  // }
-
+  List<String> num = ['1', '2', '3', '4'];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
+    return Container(
+      child: Column(
         children: [
-          ListView.builder(
-            shrinkWrap: true,
-            //scrollDirection: Axis.vertical,
-            //physics: NeverScrollableScrollPhysics(),
-            //physics: FloatingLabelBehavior.never(),
-            itemCount: data.length,
-            itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 1, horizontal: 100),
-                    margin: EdgeInsets.symmetric(vertical: 1, horizontal: 100),
-                    child: Card(
-                      child: Row(
-                        children: [
-                          Text(data[index].name),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          Text(data[index].rollnum),
-                        ],
-                      ),
-                    ),
-                  ), //Text(data[index][S_data].name),
-                ],
-              );
-            },
-          )
+          Column(
+            children: num.map((e) {
+              return Text(e);
+            }).toList(),
+          ),
+          Column(
+            children: num.map((e) => Text(e)).toList(),
+          ),
+          Column(
+            children: data.map((e) {
+              return Text(e.name);
+            }).toList(),
+          ),
         ],
       ),
     );
   }
 }
+//##################################################################################################################
+//to call and perform actions on custom class
+// class dataList extends StatefulWidget {
+//   @override
+//   _dataListState createState() => _dataListState();
+// }
+//
+// class _dataListState extends State<dataList> {
+//   List<S_data> data = [
+//     S_data(name: 'Aneeza Zafar', rollnum: '0261-BSCS-18'),
+//     S_data(name: 'Khadija Khalid', rollnum: '0265-BSCS-18'),
+//     S_data(name: 'Asma Fiaz', rollnum: '0273-BSCS-18'),
+//     S_data(name: 'Mahnoor Tariq', rollnum: '0227-BSCS-18'),
+//   ];
+//   // @override
+//   // void initState() {
+//   //   // TODO: implement initState
+//   //   S_data d = data[0];
+//   //   print(d.name);
+//   //   super.initState();
+//   // }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: Column(
+//         children: [
+//           ListView.builder(
+//             shrinkWrap: true,
+//             //scrollDirection: Axis.vertical,
+//             //physics: NeverScrollableScrollPhysics(),
+//             //physics: FloatingLabelBehavior.never(),
+//             itemCount: data.length,
+//             itemBuilder: (context, index) {
+//               return Column(
+//                 children: [
+//                   Container(
+//                     padding: EdgeInsets.symmetric(vertical: 1, horizontal: 100),
+//                     margin: EdgeInsets.symmetric(vertical: 1, horizontal: 100),
+//                     child: Card(
+//                       child: Row(
+//                         children: [
+//                           Text(data[index].name),
+//                           SizedBox(
+//                             width: 20,
+//                           ),
+//                           Text(data[index].rollnum),
+//                         ],
+//                       ),
+//                     ),
+//                   ), //Text(data[index][S_data].name),
+//                 ],
+//               );
+//             },
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
+//##################################################################################################################
 // to call identity_card class
 // class MyApp extends StatefulWidget {
 //   @override
